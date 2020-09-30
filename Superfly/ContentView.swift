991411fffe522a268f9fd5cec3ea3a33499d80da
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-29 21:30:33 -0300

    Initial Commit

---
+++ Superfly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Superfly
+//
+//  Created by Raphael Cerqueira on 29/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Superfly/SuperflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperflyApp.swift
+//  Superfly
+//
+//  Created by Raphael Cerqueira on 29/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SuperflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
