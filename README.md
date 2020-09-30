79866c82a589c94b91b7e9f57afd556b6acab046
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-30 20:30:39 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Superfly
+Curti a idéia de implementar a tela do player desse [design](https://dribbble.com/shots/14247420-Superfly-App-Redesign-Concept-2) que encontrei no Dribbble pra poder implementar o player que aparece na parte inferior. Estou fazendo tudo com SwiftUI e usando o iOS 14. Talvez seja possível reduzir para o iOS 13 já que não usei nenhum recurso novo.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/94749902-4b969880-035b-11eb-9a9d-3ab525af18d0.png" width="400">&nbsp;
+</p>
+
+## Vídeos
+[Vídeo no YouTube](https://youtu.be/wMzKInAuiwI)
+ 
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg](https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg)
+
+
+
+
+
--- Superfly/ContentView.swift
+++ Superfly/ContentView.swift
@@ -8,9 +8,150 @@
 import SwiftUI
 
 struct ContentView: View {
+    let images = ["pic1", "pic2", "header", "pic1", "pic2", "header"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                ZStack {
+                    Image("header")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    
+                    VStack {
+                        HStack {
+                            Button(action: {}, label: {
+                                Image(systemName: "arrow.turn.right.up")
+                                    .rotationEffect(Angle.degrees(-90))
+                                    .frame(width: 50, height: 50)
+                                    .background(Color.white)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                                    .clipShape(Circle())
+                            })
+                            
+                            Spacer()
+                            
+                            Button(action: {}, label: {
+                                Image(systemName: "heart")
+                                    .frame(width: 50, height: 50)
+                                    .background(Color.white)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                                    .clipShape(Circle())
+                            })
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Spacer()
+                            
+                            Circle()
+                                .frame(width: 120, height: 120)
+                                .foregroundColor(Color(#colorLiteral(red: 0.9900149703, green: 0.6732826233, blue: 0.2801778018, alpha: 1)))
+                                .overlay(
+                                    VStack(spacing: 0) {
+                                        Text("Jam")
+                                        
+                                        HStack {
+                                            Text("Alert")
+                                            
+                                            Image(systemName: "bolt.fill")
+                                        }
+                                    }
+                                    .font(.system(size: 19, weight: .bold, design: .serif))
+                                    .foregroundColor(.white)
+                                )
+                        }
+                        .padding(.trailing)
+                        .offset(y: 45)
+                    }
+                    .padding(.top, 40)
+                    .padding(.horizontal)
+                }
+                .frame(height: UIScreen.main.bounds.height / 2.2)
+                
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("Sun, 27 Sep. 2020")
+                            .font(.system(size: 17, weight: .medium))
+                            .foregroundColor(Color(#colorLiteral(red: 0.7234200835, green: 0.6854656339, blue: 0.6595992446, alpha: 1)))
+                        
+                        Text("Gregory Porter live in Concert")
+                            .font(.system(size: 42, design: .serif))
+                            .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                            .padding(.top, 20)
+                        
+                        Divider()
+                        
+                        HStack(alignment: .top) {
+                            Image(systemName: "paperplane")
+                                .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                            
+                            VStack(alignment: .leading) {
+                                Text("Stadhalle Vienna")
+                                    .font(.system(size: 17, weight: .bold))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                                
+                                Text("Roland-Rainer-Platz 1, 1150 Wien")
+                                    .font(.system(size: 17))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.4705448151, green: 0.4706160426, blue: 0.470529139, alpha: 1)))
+                            }
+                            .padding(.leading)
+                        }
+                        .padding(.top, 20)
+                        
+                        HStack {
+                            Image(systemName: "clock")
+                                .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                            
+                            Text("Sun, 27 Sept. 2020,")
+                                .font(.system(size: 17))
+                                .foregroundColor(Color(#colorLiteral(red: 0.4705448151, green: 0.4706160426, blue: 0.470529139, alpha: 1)))
+                                .padding(.leading)
+                            
+                            Text("5:00 AM MESZ")
+                                .font(.system(size: 17, weight: .bold))
+                                .foregroundColor(Color(#colorLiteral(red: 0.3607498407, green: 0.3608062863, blue: 0.3607374728, alpha: 1)))
+                        }
+                        .padding(.top, 20)
+                        
+                        Divider()
+                            .padding(.top)
+                        
+                        Text("We'd tell you Gregory Porter is back, but you probably already knew. You probably felt the earth rumbling, announcing the man's bone-deep baritone, or you felt the air change as the warmth of his sound filled the atmosphere like a hug from above. That's because All Rise, his sixth studio album, marks a return to Porter's beloved original songwriting — heart-on-sleeve")
+                            .font(.system(size: 17))
+                            .foregroundColor(Color(#colorLiteral(red: 0.4705448151, green: 0.4706160426, blue: 0.470529139, alpha: 1)))
+                            .padding(.top)
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 15) {
+                            ForEach(0 ..< images.count) { i in
+                                Image(images[i])
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 120, height: 120)
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding(.top)
+                    
+                    VStack {
+                        Text("lyrics imbued with everyday philosophy and real-life detail, set to a stirring mix of jazz, soul, blues, and gospel.")
+                            .font(.system(size: 17))
+                            .foregroundColor(Color(#colorLiteral(red: 0.4705448151, green: 0.4706160426, blue: 0.470529139, alpha: 1)))
+                            .padding(.top)
+                    }
+                    .padding(.horizontal)
+                }
+                .background(Color.white)
+                .padding(.top, 40)
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
